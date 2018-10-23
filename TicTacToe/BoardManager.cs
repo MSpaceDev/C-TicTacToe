808e48d8bec71a9b3794f7fcdc923b8674b98975
MSpaceDev <mspacedev1@gmail.com>
2018-10-23 13:09:12 +0200

    Initial commit for entire game

---
+++ TicTacToe/BoardManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+	class BoardManager
+	{
+		public string[,] Board { get; set; }
+
+		// Constructor
+		public BoardManager()
+		{
+			CreateBoard();
+		}
+
+		private void CreateBoard()
+		{
+			Board = new string[3, 3]
+			{ 
+				{ "1", "2", "3" },
+				{ "4", "5", "6" },
+				{ "7", "8", "9" }
+			};
+		}
+
+		// Redraws the board using 2D Board array
+		public void DrawBoard()
+		{
+			Console.Clear();
+			Console.WriteLine("       |       |       ");
+			Console.WriteLine("   {0}   |   {1}   |   {2}", Board[0, 0], Board[0, 1], Board[0, 2]);
+			Console.WriteLine("_______|_______|_______");
+			Console.WriteLine("       |       |       ");
+			Console.WriteLine("   {0}   |   {1}   |   {2}", Board[1, 0], Board[1, 1], Board[1, 2]);
+			Console.WriteLine("_______|_______|_______");
+			Console.WriteLine("       |       |       ");
+			Console.WriteLine("   {0}   |   {1}   |   {2}", Board[2, 0], Board[2, 1], Board[2, 2]);
+			Console.WriteLine("       |       |       ");
+			Console.WriteLine("");
+		}
+
+		// Resets the board by running the constructor
+		public void Reset()
+		{
+			CreateBoard();
+		}
+
+		// Checks if the board is full
+		public bool IsBoardFull()
+		{
+			for (int i = 0; i <= 2; i++)
+			{
+				for (int j = 0; j <= 2; j++)
+				{
+					if (Board[i, j] != "O" && Board[i, j] != "X")
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		// Checks if a player has won
+		public bool CheckWin(int[] index)
+		{
+			int x = index[0];
+			int y = index[1];
+
+			// Check vertical
+			if (Board[0,y] == Board[1,y] && Board[1, y] == Board[2, y])
+				return true;
+
+			// Check horizontal
+			if (Board[x,0] == Board[x,1] && Board[x, 1] == Board[x,2])
+				return true;
+
+			// Check diagonal 1
+			if (x == y && Board[0,0] == Board[1,1] && Board[1, 1] == Board[2,2])
+					return true;
+
+			// Check diagonal 2
+			if (x + y == 2 && Board[0,2] == Board[1,1] && Board[1, 1] == Board[2,0])
+				return true;
+
+			return false;
+		}
+
+		// Places a X or O at index given based on the player turn
+		public void PlaceMove(int[] index, int playerTurn)
+		{
+			Board[index[0], index[1]] = playerTurn == 1 ? "X" : "O";
+			DrawBoard();
+		}
+
+		// Checks if the spot at the index given has been played on
+		public bool IsSameMove(int[] index)
+		{
+			if (Board[index[0], index[1]] == "X" || Board[index[0], index[1]] == "O")
+				return true;
+
+			return false;
+		}
+	}
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+	class Program
+	{
+		static BoardManager board;
+		static int playerTurn;
+
+		static void Main(string[] args)
+		{
+			playerTurn = 1;
+
+			// Create new board
+			board = new BoardManager();
+
+			// Loop continuously until player has won
+			while (true)
+			{
+				board.DrawBoard();
+
+				// Loop until player has entered a legal value
+				string input = GetInput();
+				int[] index = new int[2];
+				while (!PlacedMove(input, out index)) {
+					input = GetInput();
+					continue;
+				}
+
+				if (board.CheckWin(index))
+				{
+					Console.WriteLine("\nPlayer {0} has won the game!\nPress any key to reset.", playerTurn);
+					Reset();
+					continue;
+				} else if (board.IsBoardFull())
+				{
+					Console.WriteLine("\nBoard is full.\nPress any key to reset.");
+					Reset();
+					continue;
+				}
+
+				// Switch between player 1 and 2
+				playerTurn = playerTurn == 1 ? 2 : 1;
+			}
+		}
+
+		private static string GetInput()
+		{
+			Console.Write("Player {0}: Choose your field! ", playerTurn);
+			return Console.ReadLine();
+		}
+
+		private static void Reset()
+		{
+			Console.ReadKey();
+			playerTurn = 1;
+			board.Reset();
+		}
+
+		private static bool PlacedMove(string input, out int[] index)
+		{
+			int value;
+			bool parse = int.TryParse(input, out value);
+
+			value = parse ? value : 0;
+			index = GetIndex(value);
+
+			// Error checking for input
+			if (value == 0)
+				Console.WriteLine("Enter numbers only!\n");
+			else if (value > 9 || value < 0)
+				Console.WriteLine("Only enter a value between 1 and 9!\n");
+			else if (board.IsSameMove(index))
+				Console.WriteLine("Only enter a square that has not been played in!\n");
+			else
+			{
+				// If there is no error with the input, place the move
+				board.PlaceMove(index, playerTurn);
+				return true;
+			}
+
+			return false;
+		}
+
+		// Converts the input 1-9 to an array of indices for the 2D array
+		private static int[] GetIndex(int value)
+		{
+			int[] indices = new int[2];
+			int counter = 0;
+
+			for (int i = 0; i <= 2; i++)
+			{
+				for (int j = 0; j <= 2; j++)
+				{
+					counter++;
+					if (counter == value)
+					{
+						indices = new int[2] { i, j };
+						break;
+					}
+				}
+			}
+			
+			return indices;
+		}
+	}
+}
